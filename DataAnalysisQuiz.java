da25714571613db04383a9067c92322cda687c3f
Ibrahim Osman <121607816+Ibrahim-Osman080@users.noreply.github.com>
2024-06-26 16:17:25 +0300

    Update DataAnalysisQuiz.java
    The code for a Data Analysis Quiz program completed

--- DataAnalysisQuiz.java
+++ DataAnalysisQuiz.java
@@ -1,3 +1,7 @@
+// Java code for a Data Analysis Quiz program completed for Unit 1 of the Programming 1 (CS1102-T5) course at the University of the People (UoPeople)
+//in AY2024-T5.The program simulates a simple quiz with multiple-choice questions and calculates the user's score.
+
+
 import java.util.Scanner;
 
 public class DataAnalysisQuiz {
